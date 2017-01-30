c3b822ce5cad6a89eeb88d11eaa2c0a928c38dae
Loïc Formont <loic.formont@gmail.com>
2017-01-30 12:18:04 +0100

    Release v1.0.13

--- index.js
+++ index.js
@@ -70,6 +70,8 @@ function defaultType(types, type) {
     } else {
         if (type.toLowerCase() === 'guid') {
             return '00000000-0000-0000-0000-000000000000';
+        } else if (type.toLowerCase() === 'string') {
+            return 'foobar';
         } else if (type.toLowerCase() === 'int') {
             return 0;
         } else if (type.toLowerCase() === 'boolean') {
